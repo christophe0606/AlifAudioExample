2b2b17f00cc754903b8a885dd634794ddeb6de69
Christophe Favergeon <Christophe.Favergeon@arm.com>
2025-10-14 15:59:20 +0200

    Added libspeex library to the project.
    It is the customized version (with some Helium optimization)
    coming from a benchmark.

---
+++ lib/speexdsp/include/os_support_custom.h
@@ -0,0 +1,28 @@
+#ifndef _OS_SUPPORT_CUSTOM_H_
+#define _OS_SUPPORT_CUSTOM_H_
+
+#ifdef   __cplusplus
+extern "C"
+{
+#endif
+
+extern void reset_speex_memory_stats();
+extern int get_speex_allocated_memory();
+extern void record_new_speex_allocation(int nb);
+
+static inline void *speex_alloc (int size)
+{
+   /* WARNING: this is not equivalent to malloc(). If you want to use malloc()
+      or your own allocator, YOU NEED TO CLEAR THE MEMORY ALLOCATED. Otherwise
+      you will experience strange bugs */
+   //record_new_speex_allocation(size);
+   return calloc(size,1);
+}
+
+#ifdef   __cplusplus
+}
+#endif
+
+
+
+#endif
